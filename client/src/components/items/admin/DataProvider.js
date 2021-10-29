f6b2c90f81ad3ad965c487e6dd77e36c3a94a88d
Zworld <92575518+zworldinc@users.noreply.github.com>
2021-10-29 18:55:59 +0530

    u

--- client/src/components/items/admin/DataProvider.js
+++ client/src/components/items/admin/DataProvider.js
@@ -1,7 +1,9 @@
 import { fetchUtils } from 'react-admin';
 import { stringify } from 'query-string';
 
-const apiUrl = 'http://localhost:3000/api';
+//const apiUrl = 'http://localhost:3000/api';
+const apiUrl = 'https://niramoy-admin.herokuapp.com/api';
+
 const httpClient = fetchUtils.fetchJson;
 
 export default {
